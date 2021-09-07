5f025e3e3cc70471e7fee4bb84bed74f46c2b61b
angel300796 <angel_hm30@hotmail.com>
2021-09-06 20:17:10 -0500

    Initialize feature/ExposeResolver

---
+++ src/resolvers/index.ts
@@ -0,0 +1,18 @@
+import { Subscription } from './Subscription'
+import { User } from './User'
+
+const path = require("path");
+const { mergeResolvers, fileLoader } = require("merge-graphql-schemas");
+
+const resolversQuery = fileLoader(path.join(__dirname, "./Query/**/*.ts"));
+const Query = mergeResolvers(resolversQuery);
+
+const resolversMutation = fileLoader(path.join(__dirname, "./Mutation/**/*.ts"));
+const Mutation = mergeResolvers(resolversMutation);
+
+export default {
+  Query,
+  Mutation,
+  Subscription,
+  User,
+};
